762ab709b29740941f7e9c74c9d16b06781d23ba
devshum <57689318+devshum@users.noreply.github.com>
2021-09-05 16:03:58 +0300

    lesson-17 built in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 interface Passenger {
   id: number,
   fullName: string,
-  checkedIn: boolean
+  checkedIn: boolean,
+  checkInDate: number | null
+  // checkInDate?: number if null doesn't exist
 }
 @Component({
   selector: 'app-root',
@@ -18,44 +20,10 @@ interface Passenger {
             [class.checked-in]="passenger.checkedIn">
           </span>
           {{i + 1}}: {{ passenger.fullName }}
-        </li>
-      </ul>
-
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span 
-            class="status"
-            [ngClass]="{
-              'checked-in': passenger.checkedIn,
-              'checked-out': !passenger.checkedIn
-            }">
-          </span>
-          {{i + 1}}: {{ passenger.fullName }}
-        </li>
-      </ul>
-
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span 
-            class="status"
-            [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')">
-          </span>
-          {{i + 1}}: {{ passenger.fullName }}
-        </li>
-      </ul>
-
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index;">
-          <span 
-            class="status"
-            [ngStyle]="{
-              backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b')
-            }">
-          </span>
-          {{i + 1}}: {{ passenger.fullName }}
+          <p>{{ passenger | json }}</p>
+          <div class="date">
+            Check in date: {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
+          </div>
         </li>
       </ul>
     </div>
@@ -66,27 +34,32 @@ export class AppComponent {
     {
       id: 1,
       fullName: 'Stephen',
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 139074200000
     },
     {
       id: 2,
       fullName: 'Rose',
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null
     },
     {
       id: 3,
       fullName: 'James',
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1391606000000
     },
     {
       id: 4,
       fullName: 'Louise',
-      checkedIn: true
+      checkedIn: true,
+      checkInDate: 1488412800000
     },
     {
       id: 5,
       fullName: 'Tina',
-      checkedIn: false
+      checkedIn: false,
+      checkInDate: null
     }
   ];
 }
