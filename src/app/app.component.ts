ba79ded2b346751be794798d809b363b72b2658a
devshum <57689318+devshum@users.noreply.github.com>
2021-09-05 16:34:10 +0300

    lesson-18 improve interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string,
+  age: number
+}
 interface Passenger {
   id: number,
   fullName: string,
   checkedIn: boolean,
-  checkInDate: number | null
+  checkInDate: number | null,
+  children: Child[] | null
   // checkInDate?: number if null doesn't exist
 }
 @Component({
@@ -20,10 +25,12 @@ interface Passenger {
             [class.checked-in]="passenger.checkedIn">
           </span>
           {{i + 1}}: {{ passenger.fullName }}
-          <p>{{ passenger | json }}</p>
-          <div class="date">
-            Check in date: {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
-          </div>
+          <span class="date">
+            Check in date: {{ passenger.checkInDate ? (passenger.checkInDate | date: 'yMMMMd') : 'Not checked in' }}
+          </span>
+          <span class="children">
+            Children: {{ passenger.children?.length || 0}}
+          </span>
         </li>
       </ul>
     </div>
@@ -35,31 +42,36 @@ export class AppComponent {
       id: 1,
       fullName: 'Stephen',
       checkedIn: true,
-      checkInDate: 139074200000
+      checkInDate: 139074200000,
+      children: null
     },
     {
       id: 2,
       fullName: 'Rose',
       checkedIn: false,
-      checkInDate: null
+      checkInDate: null,
+      children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7}]
     },
     {
       id: 3,
       fullName: 'James',
       checkedIn: true,
-      checkInDate: 1391606000000
+      checkInDate: 1391606000000,
+      children: null
     },
     {
       id: 4,
       fullName: 'Louise',
       checkedIn: true,
-      checkInDate: 1488412800000
+      checkInDate: 1488412800000,
+      children: [{ name: 'Jessica', age: 1 }]
     },
     {
       id: 5,
       fullName: 'Tina',
       checkedIn: false,
-      checkInDate: null
+      checkInDate: null,
+      children: null
     }
   ];
 }
