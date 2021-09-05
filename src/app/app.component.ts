e11ca5b18929dd711edaa6357beec2060f45ba2e
devshum <57689318+devshum@users.noreply.github.com>
2021-09-05 15:43:22 +0300

    lesson 15 & 16 different watys of styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,48 @@ interface Passenger {
       <h3>Airline Passengers</h3>
       <ul>
         <li *ngFor="let passenger of passengers; let i = index;">
+          <span 
+            class="status"
+            [class.checked-in]="passenger.checkedIn">
+          </span>
+          {{i + 1}}: {{ passenger.fullName }}
+        </li>
+      </ul>
+
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span 
+            class="status"
+            [ngClass]="{
+              'checked-in': passenger.checkedIn,
+              'checked-out': !passenger.checkedIn
+            }">
+          </span>
+          {{i + 1}}: {{ passenger.fullName }}
+        </li>
+      </ul>
+
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span 
+            class="status"
+            [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')">
+          </span>
+          {{i + 1}}: {{ passenger.fullName }}
+        </li>
+      </ul>
+
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span 
+            class="status"
+            [ngStyle]="{
+              backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b')
+            }">
+          </span>
           {{i + 1}}: {{ passenger.fullName }}
         </li>
       </ul>
