063c0b5c2e3c8bf932e6057a24e3c447a0a7cb9c
devshum <57689318+devshum@users.noreply.github.com>
2021-09-13 21:44:49 +0300

    lesson 27 @Output

--- src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ src/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
 @Component({
@@ -10,20 +10,55 @@ import { Passenger } from '../../models/passenger.interface';
         class="status"
         [class.checked-in]="detail.checkedIn">
       </span>
-      {{ detail.fullName }}
+      <div *ngIf="editing">
+        <input 
+          type="text" 
+          [value]="detail.fullName"
+          (input)="onNameChange(name.value)"
+          #name>
+      </div>
+      <div *ngIf="!editing">
+        {{ detail.fullName }}
+      </div>
       <span class="date">
         Check in date: {{ detail.checkInDate ? (detail.checkInDate | date: 'yMMMMd') : 'Not checked in' }}
       </span>
       <span class="children">
         Children: {{ detail.children?.length || 0}}
       </span>
+      <button (click)="toggleEdit()">
+        {{ editing ? 'Done' : 'Edit'}}
+      </button>
+      <button (click)="onRemove()">
+        Remove
+      </button>
     </div>
   `
 })
 
 export class PassengerDetailComponent {
   @Input() detail: Passenger;
-  constructor() {
-    
+
+  @Output() remove: EventEmitter<any> = new EventEmitter();
+  @Output() edit: EventEmitter<any> = new EventEmitter();
+
+  editing: boolean = false;
+
+  constructor() {}
+
+  onNameChange(value: string) {
+    this.detail.fullName = value;
+  }
+
+  toggleEdit() {
+    if(this.editing) {
+      this.edit.emit(this.detail);
+    }
+
+    this.editing = !this.editing;
+  }
+
+  onRemove() {
+    this.remove.emit(this.detail);
   }
 }
