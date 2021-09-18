71a06426ff2b1d8db4f5a194e9ff739a9eae4b08
devshum <57689318+devshum@users.noreply.github.com>
2021-09-18 20:49:40 +0300

    set up a passenger viewer component

---
+++ src/app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -0,0 +1,25 @@
+import { PassengerDashboardService } from './../../passenger-dashboard.service';
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Passenger } from '../../models/passenger.interface';
+
+@Component({
+  selector: 'passenger-viewer',
+  styleUrls: ['passenger-viewer.component.scss'],
+  template: `
+    <div>
+      {{ passenger | json }}
+    </div>
+  `
+})
+
+export class PassengerViewerComponent implements OnInit {
+  passenger: Passenger;
+  constructor(private passengerService: PassengerDashboardService) {}
+  ngOnInit() {
+      this.passengerService
+          .getPassneger(1)
+          .subscribe((data: Passenger) => this.passenger = data)
+  }
+}
+
