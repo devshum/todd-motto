ceb19a643bb640a68d6da545820544b069af9ec2
devshum <57689318+devshum@users.noreply.github.com>
2021-09-18 21:30:12 +0300

    lesson 38 set up form

--- src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,3 +1,4 @@
+import { PassengerFormCompoment } from './components/passenger-form/passenger-form.component';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { CommonModule } from '@angular/common';
@@ -6,16 +7,19 @@ import { PassengerCountComponent } from './components/passenger-count/passenger-
 import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
 import { PassengerDashboardService } from './passenger-dashboard.service';
 import { PassengerViewerComponent } from './containers/passenger-viewer/passenger-viewer.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     PassengerDashboardComponent,
     PassengerCountComponent,
     PassengerDetailComponent,
-    PassengerViewerComponent
+    PassengerViewerComponent,
+    PassengerFormCompoment
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   exports: [
     PassengerViewerComponent
